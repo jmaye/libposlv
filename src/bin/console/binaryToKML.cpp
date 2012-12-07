664d1b6d82ef8cb3872951dba25d081e6eb683d7
Jerome Maye <jerome.maye@mavt.ethz.ch>
2012-12-07 12:13:06 +0000

    KML done

--- src/bin/console/binaryToKML.cpp
+++ src/bin/console/binaryToKML.cpp
@@ -21,8 +21,11 @@
            into a KML file.
   */
 
+#include <cmath>
+
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
 #include "types/Packet.h"
 #include "sensor/BinaryLogReader.h"
@@ -32,63 +35,7 @@
 #include "types/Packet.h"
 #include "base/Timestamp.h"
 
-//int main(int argc, char** argv) {
-//  if (argc != 3) {
-//    std::cerr << "Usage: " << argv[0] << " <LogFilename>" << " <OutFilename>"
-//      << std::endl;
-//    return -1;
-//  }
-//  std::ifstream logFile(argv[1]);
-//  BinaryLogReader logReader(logFile);
-//  logFile.seekg (0, std::ios::end);
-//  const int length = logFile.tellg();
-//  logFile.seekg (0, std::ios::beg);
-//  std::ofstream outFile(argv[2]);
-//  outFile << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << std::endl
-//    << "<kml xmlns=\"http://www.opengis.net/kml/2.2\"" << std::endl
-//    << " xmlns:gx=\"http://www.google.com/kml/ext/2.2\">" << std::endl
-//    << "<Folder>" << std::endl
-//    << "  <Placemark>" << std::endl
-//    << "    <gx:Track>" << std::endl;
-//  std::stringstream when;
-//  std::stringstream coord;
-//  size_t packetCounter = 0;
-//  while (logFile.tellg() != length) {
-//    std::cout << std::fixed
-//      << logFile.tellg() / (double)length * 100 << " %" << '\r';
-//    double timestamp;
-//    logReader >> timestamp;
-//    try {
-//      std::shared_ptr<Packet> packet = logReader.readPacket();
-//      if (packet->instanceOfGroup()) {
-//        const Group& group = packet->groupCast();
-//        if (group.instanceOf<VehicleNavigationSolution>()) {
-//          const VehicleNavigationSolution& msg =
-//            group.typeCast<VehicleNavigationSolution>();
-//          std::string date = Timestamp(timestamp).getDate();
-//          date[10] = 'T';
-//          packetCounter++;
-//          std::stringstream packetCounterString;
-//          packetCounterString << "." << packetCounter % 100;
-//          date.append(packetCounterString.str());
-//          date.push_back('Z');
-//          when << "      <when>" << date << "</when>" << std::endl;
-//          coord << "      <gx:coord>" << std::setprecision(16)
-//            << msg.mLongitude << " " << msg.mLatitude << " "
-//            << msg.mAltitude << "</gx:coord>" << std::endl;
-//        }
-//      }
-//    }
-//    catch (IOException& e) {
-//    }
-//  }
-//  outFile << when.str() << coord.str();
-//  outFile << "    </gx:Track>" << std::endl
-//    << "  </Placemark>" << std::endl
-//    << "</Folder>" << std::endl
-//    << "</kml>";
-//  return 0;
-//}
+#define GOOGLE_EARTH_MAX_POINTS 64000
 
 int main(int argc, char** argv) {
   if (argc != 3) {
@@ -103,22 +50,22 @@ int main(int argc, char** argv) {
   logFile.seekg (0, std::ios::beg);
   std::ofstream outFile(argv[2]);
   outFile << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << std::endl
-    << "<kml xmlns=\"http://www.opengis.net/kml/2.2\">" << std::endl
-    << "  <Document>" << std::endl
-    << "    <name>" << "Applanix Run" << "</name>" << std::endl
-    << "    <description>" << "Applanix run" << "</description>" << std::endl
+    << "<kml xmlns=\"http://www.opengis.net/kml/2.2\"" << std::endl
+    << " xmlns:gx=\"http://www.google.com/kml/ext/2.2\">" << std::endl
+    << "<Folder>" << std::endl
+    << "  <Placemark>" << std::endl
     << "    <Style>" << std::endl
-    << "      <PolyStyle>" << std::endl
-    << "        <fill>0</fill>" << std::endl
-    << "        <outline>1</outline>" << std::endl
-    << "      </PolyStyle>" << std::endl
+    << "      <LineStyle>" << std::endl
+    << "        <color>FF0000E6</color>" << std::endl
+    << "        <width>2</width>" << std::endl
+    << "      </LineStyle>" << std::endl
     << "    </Style>" << std::endl
-    << "    <Placemark>" << std::endl
-    << "      <name>Applanix Run</name>" << std::endl
-    << "      <description>Applanix run</description>" << std::endl
-    << "      <MultiGeometry>" << std::endl
-    << "        <LineString>" << std::endl
-    << "          <coordinates>";
+    << "    <name>Toyota PRIUS</name>" << std::endl
+    << "    <description>Current pose of the PRIUS</description>" << std::endl
+    << "    <gx:Track>" << std::endl;
+  std::vector<std::string> when;
+  std::vector<std::string> coord;
+  std::vector<std::string> angles;
   while (logFile.tellg() != length) {
     std::cout << std::fixed
       << logFile.tellg() / (double)length * 100 << " %" << '\r';
@@ -131,19 +78,34 @@ int main(int argc, char** argv) {
         if (group.instanceOf<VehicleNavigationSolution>()) {
           const VehicleNavigationSolution& msg =
             group.typeCast<VehicleNavigationSolution>();
-          outFile << std::setprecision(16) << msg.mLongitude
-            << "," << msg.mLatitude << " ";
+          std::string date = Timestamp(msg.mTimeDistance.mTime1).getDate();
+          date[10] = 'T';
+          date.push_back('Z');
+          std::stringstream whenss;
+          std::stringstream coordss;
+          std::stringstream anglesss;
+          whenss << "      <when>" << date << "</when>" << std::endl;
+          coordss << "      <gx:coord>" << std::setprecision(16)
+            << msg.mLongitude << " " << msg.mLatitude << " "
+            << msg.mAltitude << "</gx:coord>" << std::endl;
+          anglesss << "      <gx:angles>" << msg.mHeading << " "
+            << msg.mPitch << " " << msg.mRoll << "</gx:angles>" << std::endl;
+          when.push_back(whenss.str());
+          coord.push_back(coordss.str());
+          angles.push_back(anglesss.str());
         }
       }
     }
     catch (IOException& e) {
     }
   }
-  outFile << "</coordinates>" << std::endl
-    << "        </LineString>" << std::endl
-    << "      </MultiGeometry>" << std::endl
-    << "    </Placemark>" << std::endl
-    << "  </Document>" << std::endl
+  const size_t subSample = ceil(when.size() / (double)GOOGLE_EARTH_MAX_POINTS);
+  for (size_t i = 0; i < when.size(); i += subSample) {
+    outFile << when[i] << coord[i] << angles[i];
+  }
+  outFile << "    </gx:Track>" << std::endl
+    << "  </Placemark>" << std::endl
+    << "</Folder>" << std::endl
     << "</kml>";
   return 0;
 }
